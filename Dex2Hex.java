7ade4564dfc97f5333757255964044d3185073b5
adamhanno <ahano@ymail.com>
2023-11-12 17:18:14 +0000

    Java file corrected

--- Dex2Hex.java
+++ Dex2Hex.java
@@ -16,3 +16,15 @@ class Dec2Hex
         System.out.println("Converting the Decimal Value " + num + " to Hex...");
 
         while(num != 0)
+        {
+            rem=num%16;
+            hexadecimal= ch[rem] + hexadecimal;
+            num= num/16;
+        }
+
+        System.out.println("Hexadecimal representation is: " + hexadecimal);
+
+    }
+}
+
+
